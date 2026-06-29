148806f7b0ff5f19c871ae25a51109a50574641b
Rajkumar prasad <rajkumarprasadkth@gmail.com>
2026-06-29 10:27:22 +0530

    first commit

---
+++ src/main/java/com/expenseiq/ExpenseiqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.expenseiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseiqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseiqBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expenseiq/ExpenseiqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenseiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseiqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
